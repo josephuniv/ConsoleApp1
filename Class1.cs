7eb092702a397b79f4f0580a2347aa1ef8d98103
User1 <User1@DESKTOP-G9H5SGS>
2022-05-18 15:54:38 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	
+
+
+		public void fun()
+	{
+
+
+		Console.WriteLine("Hello, World!jjjjsssjjj");
+	}
+	}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Woffffrld!");
+Class1 x=new Class1();
+x.fun();
+            //
+        }
+    }
+}
